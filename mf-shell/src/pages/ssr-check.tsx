df8bc3c633c11401f279c86afa2fb59a34d0b204
Sergiy Krasnytskyi <s.krasnitskiy@gmail.com>
2025-12-03 12:34:12 -0700

    fix: add ssr component for next on pages worker

---
+++ mf-shell/src/pages/ssr-check.tsx
@@ -0,0 +1,7 @@
+export async function getServerSideProps() {
+  return { props: { time: Date.now() } };
+}
+
+export default function SSRCheck({ time }: { time: string }) {
+  return <div>SSR OK: {time}</div>;
+}
