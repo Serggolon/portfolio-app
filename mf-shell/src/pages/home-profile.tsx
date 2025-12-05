af5c78e44d74262ddae98bb9011d0464e032a90e
Sergiy Krasnytskyi <s.krasnitskiy@gmail.com>
2025-12-04 21:03:52 -0700

    feat: remove remote import

--- mf-shell/src/pages/home-profile.tsx
+++ mf-shell/src/pages/home-profile.tsx
@@ -1,16 +1,16 @@
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
-const HomeProfileRemote = dynamic(
-	// @ts-ignore - module is provided at runtime via Module Federation
-	() => import('homeProfile/HomeProfile'),
-	{ ssr: false } // обязательно выключаем SSR
-)
+// const HomeProfileRemote = dynamic(
+// 	// @ts-ignore - module is provided at runtime via Module Federation
+// 	() => import('homeProfile/HomeProfile'),
+// 	{ ssr: false } // обязательно выключаем SSR
+// )
 
-export default function TodoStatsPage() {
-	return (
-		<div>
-			<h1>Todo Stats (remote)</h1>
-			<HomeProfileRemote />
-		</div>
-	)
-}
+// export default function HomeProfilePage() {
+// 	return (
+// 		<div>
+// 			<h1>Todo Stats (remote)</h1>
+// 			<HomeProfileRemote />
+// 		</div>
+// 	)
+// }
