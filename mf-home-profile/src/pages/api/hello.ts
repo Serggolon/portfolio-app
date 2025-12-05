8e8c1dbc417cfc356903df9965c377ab69a20c6b
Sergiy Krasnytskyi <s.krasnitskiy@gmail.com>
2025-12-04 20:27:55 -0700

    fix: code refactor

--- mf-home-profile/src/pages/api/hello.ts
+++ mf-home-profile/src/pages/api/hello.ts
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export const runtime = "edge";
-
 type Data = {
   name: string;
 };
@@ -13,3 +11,5 @@ export default function handler(
 ) {
   res.status(200).json({ name: "John Doe" });
 }
+
+export const runtime = "edge";
